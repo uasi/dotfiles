046746089ab485c0bc9044fdf468c0f8210f6405
Tomoki Aonuma <uasi@uasi.jp>
2019-09-17 19:47:17 +0900

    [zsh] Set GIT_WORK_TREE by hook

--- .zsh/rc/hooks.zsh
+++ .zsh/rc/hooks.zsh
@@ -4,12 +4,14 @@ typeset -a precmd_functions
 _chpwd_dotfiles_git_hook() {
     if [[ "$PWD" = "$HOME" ]]; then
         if [[ -z "$GIT_DIR" && -e "$HOME/.dotfiles.git" ]]; then
-            echo "chpwd: Set GIT_DIR"
+            echo "chpwd: Set GIT_DIR and GIT_WORK_TREE"
             export GIT_DIR=$HOME/.dotfiles.git
+            export GIT_WORK_TREE=$HOME
         fi
     else
-        echo "chpwd: Unset GIT_DIR"
+        echo "chpwd: Unset GIT_DIR and GIT_WORK_TREE"
         unset GIT_DIR
+        unset GIT_WORK_TREE
     fi
 }
 
