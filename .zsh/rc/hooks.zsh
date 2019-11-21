1fdbe4bcfe17d284218b4f284d558b339abd00b7
Tomoki Aonuma <uasi@uasi.jp>
2019-11-21 14:44:18 +0900

    [git] Update hooks

--- .zsh/rc/hooks.zsh
+++ .zsh/rc/hooks.zsh
@@ -4,12 +4,12 @@ typeset -a precmd_functions
 _chpwd_dotfiles_git_hook() {
     if [[ "$PWD" = "$HOME" ]]; then
         if [[ -z "$GIT_DIR" && -e "$HOME/.dotfiles.git" ]]; then
-            echo "chpwd: Set GIT_DIR and GIT_WORK_TREE"
+            echo "chpwd: Entered HOME. Set git env for .dotfiles.git."
             export GIT_DIR=$HOME/.dotfiles.git
             export GIT_WORK_TREE=$HOME
         fi
-    else
-        echo "chpwd: Unset GIT_DIR and GIT_WORK_TREE"
+    elif [[ "$OLDPWD" = "$HOME" ]]; then
+        echo "chpwd: Left HOME. Unset git env for .dotfiles.git."
         unset GIT_DIR
         unset GIT_WORK_TREE
     fi
