5d2fcfc909561b87b417dbadaf3a76d9d57d1293
Tomoki Aonuma <uasi@uasi.jp>
2019-12-24 15:53:00 +0900

    [tmux] Use built-in %if

--- .zsh/rc/application_settings.zsh
+++ .zsh/rc/application_settings.zsh
@@ -1,7 +1,3 @@
-if [[ -n "$TMUX" ]]; then
-   cut -c3- ~/.tmux.conf | sh -s on_login
-fi
-
 [[ -s "$HOME/.volta/load.sh" ]] && source "$HOME/.volta/load.sh"
 
 [[ -s "$HOME/.cpad2/profile" ]] && source "$HOME/.cpad2/profile"
