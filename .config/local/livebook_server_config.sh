fea4c4d93ce2b17969003b954d90dea2753bf223
Tomoki Aonuma <uasi@uasi.jp>
2024-01-07 01:12:00 +0900

    [local] Add livebook_server_config.sh

---
+++ .config/local/livebook_server_config.sh
@@ -0,0 +1,7 @@
+# escripts expect `escript` in PATH.
+export PATH=$HOME/.mix/escripts:/opt/homebrew/bin:$PATH
+
+export LIVEBOOK_PORT=4446
+
+export LIVEBOOK_DATA_PATH=$HOME/Dropbox/Data/Livebook/Data
+export LIVEBOOK_HOME=$HOME/Dropbox/Data/Livebook/Home
