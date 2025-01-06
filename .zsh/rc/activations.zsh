b9a46959511dc4136bcaf5d3bc7f9463aecde7f4
Tomoki Aonuma <uasi@uasi.jp>
2025-01-07 03:15:56 +0900

    [zsh] Add activation for OrbStack

--- .zsh/rc/activations.zsh
+++ .zsh/rc/activations.zsh
@@ -5,6 +5,10 @@ if has brew; then
     source "$(brew --prefix)"/opt/fzf/shell/{completion,key-bindings}.zsh 2> /dev/null
 fi
 
+if [[ -f ~/.orbstack/shell/init.zsh ]]; then
+    source ~/.orbstack/shell/init.zsh 2>/dev/null || :
+fi
+
 [[ -s "$HOME/.volta/load.sh" ]] && source "$HOME/.volta/load.sh"
 
 [[ -s "$HOME/.cpad2/profile" ]] && source "$HOME/.cpad2/profile"
