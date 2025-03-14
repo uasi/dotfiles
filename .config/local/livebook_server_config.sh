2668dbca6941c7f77e5003b02fed5693d9642ccf
Tomoki Aonuma <uasi@uasi.jp>
2025-03-14 23:40:47 +0900

    [local] Remove livebook_server_config.sh

--- .config/local/livebook_server_config.sh
+++
@@ -1,7 +0,0 @@
-# escripts expect `escript` in PATH.
-export PATH=$HOME/.mix/escripts:/opt/homebrew/bin:$PATH
-
-export LIVEBOOK_PORT=4446
-
-export LIVEBOOK_DATA_PATH=$HOME/Dropbox/Data/Livebook/Data
-export LIVEBOOK_HOME=$HOME/Dropbox/Data/Livebook/Home
