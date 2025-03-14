d83b4ee63dba9b2e3d158020fcea503974d9754d
Tomoki Aonuma <uasi@uasi.jp>
2025-03-14 23:39:06 +0900

    [local] Remove miniflux_server_config.sh

--- .config/local/miniflux_server_config.sh
+++
@@ -1,9 +0,0 @@
-export LISTEN_ADDR=127.0.0.1:65261
-export DATABASE_URL="user=miniflux dbname=miniflux sslmode=disable"
-
-# https://miniflux.app/miniflux.1.html
-
-export POLLING_SCHEDULER=entry_frequency
-
-export CLEANUP_ARCHIVE_READ_DAYS=-1
-export CLEANUP_ARCHIVE_UNREAD_DAYS=-1
